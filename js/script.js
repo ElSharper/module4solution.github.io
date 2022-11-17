1c8e3d8036ceaf70017337dac89d5a12c07221ff
Bayoud El Mehdi <118342478+ElSharper@users.noreply.github.com>
2022-11-17 10:51:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 (function () {
 
-    var names = ["Mehdi", "ELmehdi", "Ean", "Ema", "Zak", "raja", "Madi", "Sha", "rca", "Elsharper"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
       var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'e') {
+      if (firstLetter === 'j') {
         byeSpeaker.speak(names[i]);
       } 
       else {
